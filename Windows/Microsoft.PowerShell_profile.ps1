1d82445ba8c8ae8a10a71e60e29b25f1dfafef76
Avneesh Mishra <123avneesh@gmail.com>
2023-11-15 15:09:07 +0530

    Added mamba init and other powershell functions for Windows

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -22,6 +22,27 @@ Function New-File {
 }
 Set-Alias touch New-File
 
+# === Ada commands ===
+$USER_ADA = "avneesh.mishra"
+$USER_RRC = $USER_ADA
+Function Link-Gnode {
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$NodeNum,
+        [Parameter(Mandatory=$false, Position=1)]
+        [string]$LocalPort = "60000",
+        [Parameter(Mandatory=$false, Position=2)]
+        [string]$RemotePort = "22"
+    )
+    $gnode = $NodeNum.PadLeft(3, '0')
+    $ssh_keygen_cmd = "ssh-keygen -f ${HOME}\.ssh\known_hosts -R '[localhost]:$LocalPort'"
+    echo "Running: >> $ssh_keygen_cmd"
+    Invoke-Expression $ssh_keygen_cmd
+    $ssh_cmd = "ssh -t -i ${HOME}\.ssh\id_rsa_ada_win -L ${LocalPort}:localhost:${RemotePort} -J ${USER_ADA}@ada ${USER_ADA}@gnode${gnode} zsh"
+    echo "Running: >> $ssh_cmd"
+    Invoke-Expression $ssh_cmd
+}
+
 # ========== Path modifications ==========
 # Add Java path
 $JavaPath = "C:\Program Files\Java\jdk-11.0.9"
@@ -38,7 +59,16 @@ Function Conda-Init {
     (& "C:\ProgramData\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
     #endregion
 }
+Function Mamba-Init {
+    #region conda initialize
+    # !! Contents within this block are managed by 'conda init' !!
+    If (Test-Path "C:\Users\123av\miniforge3\Scripts\conda.exe") {
+        (& "C:\Users\123av\miniforge3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+    }
+    #endregion
+}
 # ----- Oh My Posh! -----
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme blue-owl
 Import-Module -Name Terminal-Icons
+Import-Module DockerCompletion
